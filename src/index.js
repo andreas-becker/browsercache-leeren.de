910c92b559989cd7a7bb34f496dcea1ecb32e62d
Andreas Becker <mail@andreasbecker.de>
2020-02-07 21:25:49 +0100

    Added basic layout and output of browser/-version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-require("typeface-open-sans")
+require("typeface-open-sans");
+const Bowser = require("bowser");
 
 import "./style.css"
+
+const browser = Bowser.getParser(window.navigator.userAgent);
+console.log(browser.parsedResult);
+document.body.classList.add(String(browser.parsedResult.os.name).toLowerCase());
+document.body.classList.add(String(browser.parsedResult.platform.type).toLowerCase());
+document.body.classList.add(String(browser.parsedResult.browser.name).toLowerCase());
+
+document.getElementById('js-browsername').innerHTML = browser.parsedResult.browser.name + ' ' + browser.parsedResult.browser.version;
